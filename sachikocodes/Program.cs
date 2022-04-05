cc7521d09705ae0cd9298763565a7c246994bca6
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-04-05 23:21:19 +0100

    third commit

---
+++ sachikocodes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sachikocodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 22 + 15;
+            int z = 83 + 44;
+            int a = 77 + 3;
+            Console.WriteLine(x > y);
+            Console.WriteLine(x > z);
+            Console.WriteLine(x > a); }
+}}
